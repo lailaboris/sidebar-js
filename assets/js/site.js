a4c01f975407dc96edf13b85de494f3fa10fdbdd
lailaboris <liloboris05@gmail.com>
2024-05-03 12:55:42 +0200

    hunde nav bar
    sidebar in js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,2 +1,14 @@
 // write cool JS hwere!!
 
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    var main = document.getElementById("mainContent");
+  
+    if (sidebar.style.width === "250px") {
+      sidebar.style.width = "0";
+      main.style.marginLeft = "0";
+    } else {
+      sidebar.style.width = "250px";
+      main.style.marginLeft = "250px";
+    }
+  }
